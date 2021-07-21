be07f17f221bb264821c329a2808b0e5cc8b6cef
透光版 <touguangban@gmail.com>
2021-07-21 11:33:55 +0800

    1

--- README.md
+++
@@ -1,1 +0,0 @@
-# touguangban.github.io
---
+++ asset/c.js
@@ -0,0 +1,86 @@
+﻿document.onkeydown = function(){
+
+    if(window.event && window.event.keyCode == 123) {
+        alert("不要扒网站啦，一首凉凉送给你");
+        event.keyCode=0;
+        event.returnValue=false;
+    }
+    if(window.event && window.event.keyCode == 13) {
+        window.event.keyCode = 505;
+    }
+    if(window.event && window.event.keyCode == 8) {
+        alert(str+"\n请使用Del键进行字符的删除操作！");
+        window.event.returnValue=false;
+    }
+
+}
+
+document.oncontextmenu = function (event){
+if(window.event){
+event = window.event;
+}try{
+var the = event.srcElement;
+if (!((the.tagName == "INPUT" && the.type.toLowerCase() == "text") || the.tagName == "TEXTAREA")){
+return false;
+}
+return true;
+}catch (e){
+return false;
+}
+}
+
+document.onpaste = function (event){
+if(window.event){
+event = window.event;
+}try{
+var the = event.srcElement;
+if (!((the.tagName == "INPUT" && the.type.toLowerCase() == "text") || the.tagName == "TEXTAREA")){
+return false;
+}
+return true;
+}catch (e){
+return false;
+}
+}
+
+document.oncopy = function (event){
+if(window.event){
+event = window.event;
+}try{
+var the = event.srcElement;
+if(!((the.tagName == "INPUT" && the.type.toLowerCase() == "text") || the.tagName == "TEXTAREA")){
+return false;
+}
+return true;
+}catch (e){
+return false;
+}
+}
+
+document.oncut = function (event){
+if(window.event){
+event = window.event;
+}try{
+var the = event.srcElement;
+if(!((the.tagName == "INPUT" && the.type.toLowerCase() == "text") || the.tagName == "TEXTAREA")){
+return false;
+}
+return true;
+}catch (e){
+return false;
+}
+}
+
+document.onselectstart = function (event){
+if(window.event){
+event = window.event;
+}try{
+var the = event.srcElement;
+if (!((the.tagName == "INPUT" && the.type.toLowerCase() == "text") || the.tagName == "TEXTAREA")){
+return false;
+}
+return true;
+} catch (e) {
+return false;
+}
+}
